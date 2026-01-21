cd5c3b84b194913b3d74a671c0343e21d0363657
JuniorNascimento777 <juniorferaea@gmail.com>
2026-01-21 04:05:09 -0300

    feat: added edit book feat: added delete book feat: added view book

--- readium/app/src/main/java/com/example/readium/repository/BookRepository.kt
+++ readium/app/src/main/java/com/example/readium/repository/BookRepository.kt
@@ -3,12 +3,16 @@ package com.example.readium.repository
 import com.example.readium.data.model.Book
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
+import android.net.Uri
+import com.google.firebase.storage.FirebaseStorage
+import androidx.core.net.toUri
 
 class BookRepository {
 
     private val firestore = FirebaseFirestore.getInstance()
     private val booksRef = firestore.collection("books")
 
+    private val storage = FirebaseStorage.getInstance()
 
     suspend fun addBook(
         book: Book,
@@ -20,8 +24,31 @@ class BookRepository {
             ownerDisplayName = userName
         )
 
-        booksRef.add(newBook).await()
+        val docRef = booksRef.add(newBook).await()
+        val bookId = docRef.id
+
+        if (!book.coverUrl.isNullOrBlank() && book.coverUrl!!.startsWith("content://")) {
+            val downloadUrl = uploadBookCover(bookId, book.coverUrl!!)
+            val finalUrl = "$downloadUrl?ts=${System.currentTimeMillis()}"
+
+            docRef.update("coverUrl", finalUrl).await()
+        }
     }
+
+
+    private suspend fun uploadBookCover(
+        bookId: String,
+        coverUri: String
+    ): String {
+        val uri = coverUri.toUri()
+
+        val ref = storage.reference.child("book_covers/$bookId.jpg")
+
+        ref.putFile(uri).await()
+
+        return ref.downloadUrl.await().toString()
+    }
+
 
     fun getUserBooks(
         userId: String,
@@ -45,11 +72,21 @@ class BookRepository {
     suspend fun updateBook(book: Book) {
         if (book.id.isBlank()) return
 
+        var updatedBook = book
+
+        if (!book.coverUrl.isNullOrBlank() && book.coverUrl!!.startsWith("content://")) {
+            val downloadUrl = uploadBookCover(book.id, book.coverUrl!!)
+            updatedBook = book.copy(
+                coverUrl = "$downloadUrl?ts=${System.currentTimeMillis()}"
+            )
+        }
+
         booksRef
             .document(book.id)
-            .set(book)
+            .set(updatedBook)
             .await()
     }
+
 
     suspend fun deleteBook(book: Book) {
         if (book.id.isBlank()) return
